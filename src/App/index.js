3e9bfc434ffea305b3df0ce9a00644a92351f055
Naveen Nata Raja <naveennraja@gmail.com>
2022-01-29 21:10:16 +0530

    Globe Commit

---
+++ src/App/index.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Globe from "./Globe"
+
+export default ()=> {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Globe></Globe>
+      </header>
+    </div>
+  );
+}
+
+// export default App;
