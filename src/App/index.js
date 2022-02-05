6dce7b1d11c23f2b1591f6b63321871b4d7725bc
Gadigesh Hiremath <gchiremath.23@gmail.com>
2022-02-05 15:57:25 +0530

    Initial commit

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Globe from "./Globe"
+import Animation from './Animation';
 
 export default ()=> {
   return (
     <div className="App">
       <header className="App-header">
         <Globe></Globe>
+        <Animation></Animation>
       </header>
     </div>
   );
